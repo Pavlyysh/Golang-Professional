27997061798bee5852713ea2e3133cf7a4ef2d77
Pavlyysh <pgfu619@mail.ru>
2025-08-04 16:19:24 +0300

    client Timeout, usage: URL(required) timeout(optional)

---
+++ chapter12/clientTimeOut/clientTimeOut.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+)
+
+var timeout = time.Duration(1 * time.Second)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Printf("Usage: %s URL TIMEOUT\n", filepath.Base(os.Args[0]))
+		return
+	}
+	if len(os.Args) == 3 {
+		temp, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Using Default Timeout")
+		} else {
+			timeout = time.Duration(time.Duration(temp) * time.Second)
+		}
+	}
+
+	URL := os.Args[1]
+	t := http.Transport{Dial: Timeout}
+
+	client := http.Client{
+		Transport: &t,
+	}
+	data, err := client.Get(URL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		defer data.Body.Close()
+		_, err := io.Copy(os.Stdout, data.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+func Timeout(network, address string) (net.Conn, error) {
+	conn, err := net.DialTimeout(network, address, timeout)
+	if err != nil {
+		return nil, err
+	}
+	conn.SetDeadline(time.Now().Add(timeout))
+	return conn, nil
+}
